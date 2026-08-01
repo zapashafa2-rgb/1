74da10e76db09ae8cb727d4a502301ea903b4166
Zaldy Aditya <zapashafa2@gmail.com>
2026-08-01 21:40:27 +0700

    carissa

--- main.js
+++ main.js
@@ -104,3 +104,34 @@ form.addEventListener("submit", async function(e){
     alert("Pesan berhasil dikirim!");
     form.reset();
 });
+///scrolll geser
+const slider = document.querySelector(".proyek");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+
+    e.preventDefault();
+
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2;
+
+    slider.scrollLeft = scrollLeft - walk;
+});
